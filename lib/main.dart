f2a2a246d47d59c2517d27baf37a3912a74aee64
saif hatem <shatem793@gmail.com>
2024-08-09 00:18:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(
+    //const BooklyApp(),
+    DevicePreview(
+      builder: (context) => const BooklyApp(),
+      enabled: !kReleaseMode,
+    ),
+  );
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
