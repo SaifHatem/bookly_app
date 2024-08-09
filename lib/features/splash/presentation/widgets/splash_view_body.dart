0982c70479bf813fe0560c8c6a253a00f6fc3a51
saif hatem <shatem793@gmail.com>
2024-08-09 04:33:30 +0300

    nabigate to home & refactor functions in init state

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/widgets/fade_animation.dart';
 import 'package:bookly_app/features/splash/presentation/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,19 +21,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
+    initSlidingAndFadeAnimation();
 
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
-            .animate(animationController);
-
-    fadeAnimation =
-        Tween<double>(begin: 0.0, end: 1).animate(animationController);
-
-    animationController.forward();
+    navigateToHomeView();
   }
 
   @override
@@ -49,5 +42,31 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAndFadeAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
+            .animate(animationController);
+
+    fadeAnimation =
+        Tween<double>(begin: 0.0, end: 1).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHomeView() {
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.to(
+        const HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
