0b0af012c623dcf5c6166ce602eece414dc172e4
Saif Hatem <shatem793@gmail.com>
2024-08-16 01:44:05 +0300

    lanch url part 2

--- lib/features/home/presentation/views/widgets/custom_buttons_actions.dart
+++ lib/features/home/presentation/views/widgets/custom_buttons_actions.dart
@@ -1,7 +1,7 @@
+import 'package:bookly_app/core/utils/functions/launch_url.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/widgets/custom_button_widget.dart';
 
@@ -37,35 +37,29 @@ class CustomButtonsActions extends StatelessWidget {
         ),
         Expanded(
           child: CustomActionButton(
-            onPressed: () async {
-              Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-              if (await canLaunchUrl(uri)) {
-                await launchUrl(uri);
-              }
+            onPressed: () {
+              launcCustomhUrl(context, "bookModel.volumeInfo.previewLink");
             },
-            // onPressed: () async {
-            //   Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-            //   log('Attempting to launch URL: $uri');
-            //   if (await canLaunchUrl(uri)) {
-            //     await launchUrl(uri);
-            //     log('URL launched successfully');
-            //   } else {
-            //     log('Cannot launch URL: $uri');
-            //   }
-            // },
-
             borderRadius: const BorderRadius.only(
               topRight: Radius.circular(12),
               bottomRight: Radius.circular(12),
             ),
             backgroundColor: const Color(0xffef8262),
-            textColor: const Text(
-              'Free preview',
-              style: TextStyle(color: Colors.white),
+            textColor: Text(
+              getText(bookModel),
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
       ],
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not available';
+    } else {
+      return 'Preview';
+    }
   }
 }
