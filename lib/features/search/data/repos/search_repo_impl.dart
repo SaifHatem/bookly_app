e38a7eb4329601bac6b0f69fdb345d4e627794ff
Saif Hatem <shatem793@gmail.com>
2024-08-16 22:54:18 +0300

    create search vrepo & search repo implementation

---
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  final ApiService apiService;
+
+  SearchRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchResult(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=sublect:$category');
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioExceptio(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
